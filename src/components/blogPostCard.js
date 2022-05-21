133de148e62b1364529555a8ed911458ac6fef63
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-05-21 22:39:04 +0900

    CREATE BlogPostCardComponent

---
+++ src/components/blogPostCard.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export const BlogPostCard = props => {
+  const { title, slug, postedDate, description } = props
+  return (
+    <article>
+      <header>
+        <h2>
+          <Link to={slug} itemProp="url">
+            <span itemProp="headline">{title}</span>
+          </Link>
+        </h2>
+        <small>{postedDate}</small>
+      </header>
+      <section>
+        <p itemProp="description">{description}</p>
+      </section>
+    </article>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import * as React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { BlogPostCard } from "../components/blogPostCard"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -30,28 +31,12 @@ const BlogIndex = ({ data, location }) => {
 
           return (
             <li key={post.fields.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                  <small>{post.frontmatter.date}</small>
-                </header>
-                <section>
-                  <p
-                    dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt,
-                    }}
-                    itemProp="description"
-                  />
-                </section>
-              </article>
+              <BlogPostCard
+                title={title}
+                slug={post.fields.slug}
+                postedDate={post.frontmatter.date}
+                description={post.frontmatter.description || post.excerpt}
+              />
             </li>
           )
         })}
