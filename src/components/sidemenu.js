d8c54f8b14480e92e73261e8b010888f0cf60275
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-05-22 09:16:41 +0900

    Delete sidemenu css

--- src/components/sidemenu.js
+++ src/components/sidemenu.js
@@ -1,10 +1,9 @@
 import React from "react"
 import Bio from "./bio"
-import { sidemenu } from "./sidemenu.module.css"
 
 export const SideMenu = () => {
   return (
-    <div className={sidemenu}>
+    <div>
       <Bio />
     </div>
   )
