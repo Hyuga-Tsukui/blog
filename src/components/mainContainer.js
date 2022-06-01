860b8da0054b89bcbd888be5e4fbbe71488ae3ff
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-06-01 22:18:57 +0900

    Add Feed

--- src/components/mainContainer.js
+++ src/components/mainContainer.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-import { mainContainer } from "./mainContainer.module.css"
-
 export const MainContainer = props => {
   const { children } = props
   return <main className="main-container">{children}</main>
