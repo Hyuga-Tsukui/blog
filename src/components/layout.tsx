716fb91b6c2af9509d2485e79b1cc690b3f4d4a6
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-06-08 00:03:46 +0900

    Add react-helmet

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,6 +10,7 @@ import {
   // @ts-ignore
 } from "./layout.module.css";
 import { StaticImage } from "gatsby-plugin-image";
+import {Helmet} from "react-helmet";
 
 type Props = {
   pageTitle: string;
@@ -33,80 +34,100 @@ export const Layout: FC<Props> = (props) => {
   `);
 
   const social = data.site?.siteMetadata?.social;
+  const title = `${pageTitle} | ${data.site?.siteMetadata?.title}`
 
   return (
-    <div className={container}>
-      <title>
-        {pageTitle} | {data.site?.siteMetadata?.title}
-      </title>
-      <header className={siteTitle}>{data.site?.siteMetadata?.title}</header>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <a
-              href={`https://twitter.com/${social?.twitter}`}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <StaticImage
-                alt="My Twitter Link"
-                src="../images/twitter-icon.svg"
-                width={24}
-                height={24}
-                quality={95}
-              />
-            </a>
-          </li>
-          <li className={navLinkItem}>
-            <a
-              href={`https://github.com/${social?.github}`}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <StaticImage
-                alt="My GitHub Link"
-                src="../images/github-icon.svg"
-                width={24}
-                height={24}
-                quality={95}
-              />
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-      <footer>
-        <p>
-          <small>
-            ©　Hyuga-Tsukui created-at 2022
-            <br />
-            このサイトはGoogle Analyticsを使用しています。
-            <a
-              rel="noopener noreferrer"
-              target="_blank"
-              href="https://policies.google.com/technologies/partner-sites?hl=ja"
-            >
-              詳しく見る
-            </a>
-          </small>
-        </p>
-        <p>
-          <small></small>
-        </p>
-      </footer>
-    </div>
+    <>
+      <Helmet
+        htmlAttributes={{
+          lang: 'ja'
+        }}
+        title={title}
+        meta={[
+          {
+            name: `twitter:card`,
+            content: `sumary`
+          },
+          {
+            name: `twitter:creator`,
+            content: data.site?.siteMetadata?.social?.twitter || ``,
+          },
+          {
+            name: `twitter:title`,
+            content: title,
+          },
+        ]}
+      />
+      <div className={container}>
+        <header className={siteTitle}>{data.site?.siteMetadata?.title}</header>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/about" className={navLinkText}>
+                About
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <a
+                href={`https://twitter.com/${social?.twitter}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <StaticImage
+                  alt="My Twitter Link"
+                  src="../images/twitter-icon.svg"
+                  width={24}
+                  height={24}
+                  quality={95}
+                />
+              </a>
+            </li>
+            <li className={navLinkItem}>
+              <a
+                href={`https://github.com/${social?.github}`}
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <StaticImage
+                  alt="My GitHub Link"
+                  src="../images/github-icon.svg"
+                  width={24}
+                  height={24}
+                  quality={95}
+                />
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+        <footer>
+          <p>
+            <small>
+              ©　Hyuga-Tsukui created-at 2022
+              <br />
+              このサイトはGoogle Analyticsを使用しています。
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href="https://policies.google.com/technologies/partner-sites?hl=ja"
+              >
+                詳しく見る
+              </a>
+            </small>
+          </p>
+          <p>
+            <small></small>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
