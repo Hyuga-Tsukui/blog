84f5a0b2bd25c211ba2d00c0a47710748e5a4aca
Hyuga-Tsukui <gakikiki407@gmail.com>
2022-06-08 22:45:40 +0900

    Add Seo Component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -10,7 +10,6 @@ import {
   // @ts-ignore
 } from "./layout.module.css";
 import { StaticImage } from "gatsby-plugin-image";
-import {Helmet} from "react-helmet";
 
 type Props = {
   pageTitle: string;
@@ -34,100 +33,77 @@ export const Layout: FC<Props> = (props) => {
   `);
 
   const social = data.site?.siteMetadata?.social;
-  const title = `${pageTitle} | ${data.site?.siteMetadata?.title}`
 
   return (
-    <>
-      <Helmet
-        htmlAttributes={{
-          lang: 'ja'
-        }}
-        title={title}
-        meta={[
-          {
-            name: `twitter:card`,
-            content: `sumary`
-          },
-          {
-            name: `twitter:creator`,
-            content: data.site?.siteMetadata?.social?.twitter || ``,
-          },
-          {
-            name: `twitter:title`,
-            content: title,
-          },
-        ]}
-      />
-      <div className={container}>
-        <header className={siteTitle}>{data.site?.siteMetadata?.title}</header>
-        <nav>
-          <ul className={navLinks}>
-            <li className={navLinkItem}>
-              <Link to="/" className={navLinkText}>
-                Home
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <Link to="/about" className={navLinkText}>
-                About
-              </Link>
-            </li>
-            <li className={navLinkItem}>
-              <a
-                href={`https://twitter.com/${social?.twitter}`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <StaticImage
-                  alt="My Twitter Link"
-                  src="../images/twitter-icon.svg"
-                  width={24}
-                  height={24}
-                  quality={95}
-                />
-              </a>
-            </li>
-            <li className={navLinkItem}>
-              <a
-                href={`https://github.com/${social?.github}`}
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                <StaticImage
-                  alt="My GitHub Link"
-                  src="../images/github-icon.svg"
-                  width={24}
-                  height={24}
-                  quality={95}
-                />
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <main>
-          <h1 className={heading}>{pageTitle}</h1>
-          {children}
-        </main>
-        <footer>
-          <p>
-            <small>
-              ©　Hyuga-Tsukui created-at 2022
-              <br />
-              このサイトはGoogle Analyticsを使用しています。
-              <a
-                rel="noopener noreferrer"
-                target="_blank"
-                href="https://policies.google.com/technologies/partner-sites?hl=ja"
-              >
-                詳しく見る
-              </a>
-            </small>
-          </p>
-          <p>
-            <small></small>
-          </p>
-        </footer>
-      </div>
-    </>
+    <div className={container}>
+      <header className={siteTitle}>{data.site?.siteMetadata?.title}</header>
+      <nav>
+        <ul className={navLinks}>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/about" className={navLinkText}>
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <a
+              href={`https://twitter.com/${social?.twitter}`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <StaticImage
+                alt="My Twitter Link"
+                src="../images/twitter-icon.svg"
+                width={24}
+                height={24}
+                quality={95}
+              />
+            </a>
+          </li>
+          <li className={navLinkItem}>
+            <a
+              href={`https://github.com/${social?.github}`}
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <StaticImage
+                alt="My GitHub Link"
+                src="../images/github-icon.svg"
+                width={24}
+                height={24}
+                quality={95}
+              />
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h1 className={heading}>{pageTitle}</h1>
+        {children}
+      </main>
+      <footer>
+        <p>
+          <small>
+            ©　Hyuga-Tsukui created-at 2022
+            <br />
+            このサイトはGoogle Analyticsを使用しています。
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://policies.google.com/technologies/partner-sites?hl=ja"
+            >
+              詳しく見る
+            </a>
+          </small>
+        </p>
+        <p>
+          <small></small>
+        </p>
+      </footer>
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { Layout } from "../components/layout";
 import { graphql, Link } from "gatsby";
+import Seo from "../components/seo";
 
 type Props = {
   data: GatsbyTypes.MyBlogsQuery;
@@ -9,20 +10,23 @@ type Props = {
 const BlogPage: FC<Props> = (props) => {
   const { data } = props;
   return (
-    <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.slug}`}>
-                {node.frontmatter?.title || "NON TITLE"}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter?.date}</p>
-          </article>
-        ))}
-      </ul>
-    </Layout>
+    <>
+      <Seo />
+      <Layout pageTitle="My Blog Posts">
+        <ul>
+          {data.allMdx.nodes.map((node) => (
+            <article key={node.id}>
+              <h2>
+                <Link to={`/blog/${node.slug}`}>
+                  {node.frontmatter?.title || "NON TITLE"}
+                </Link>
+              </h2>
+              <p>Posted: {node.frontmatter?.date}</p>
+            </article>
+          ))}
+        </ul>
+      </Layout>
+    </>
   );
 };
 
